19f5a4abc67391f339fd47955752fdbc155cfb6a
kevel choumene <k.choumene@cloudfacile.com>
2025-05-04 13:21:49 +0100

    feat(Generate invoice): ajustement qte vu que je dive maintenant le montant

--- contribution/services.py
+++ contribution/services.py
@@ -367,11 +367,9 @@ def update_or_create_premium(premium, user, action=None):
                                 if premium.policy.periodicity == 'Q':
                                     family_amount = family_amount / 3
                                     government_amount = government_amount / 3
-                                    quantity = 3
                                 elif premium.policy.periodicity == 'S':
                                     family_amount = family_amount / 6
                                     government_amount = government_amount / 6
-                                    quantity = 6
                                 elif premium.policy.periodicity == 'Y':
                                     family_amount = family_amount / 12
                                     government_amount = government_amount / 12
