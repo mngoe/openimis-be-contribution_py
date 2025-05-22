6b9cee67e83e874d509d1016f979a98420037199
kevel choumene <k.choumene@cloudfacile.com>
2025-05-22 11:30:07 +0100

    feat(roolback invoice on contribution): roollback feature-31168

--- contribution/apps.py
+++ contribution/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-import inspect, importlib
 
 MODULE_NAME = "contribution"
 
@@ -8,17 +7,8 @@ DEFAULT_CFG = {
     "gql_mutation_create_premiums_perms": ["101302"],
     "gql_mutation_update_premiums_perms": ["101303"],
     "gql_mutation_delete_premiums_perms": ["101304"],
-    "generate_invoice_on_contribution": False
 }
 
-CALCULATION_RULES = []
-
-def read_all_calculation_rules():
-    """function to read all calculation rules"""
-    for name, cls in inspect.getmembers(importlib.import_module("calculation_comores.calculation_rule"), inspect.isclass):
-        if 'calculation' in cls.__module__.split('.')[0]:
-            CALCULATION_RULES.append(cls)
-            cls.ready()
 
 class ContributionConfig(AppConfig):
     name = MODULE_NAME
@@ -27,7 +17,6 @@ class ContributionConfig(AppConfig):
     gql_mutation_create_premiums_perms = []
     gql_mutation_update_premiums_perms = []
     gql_mutation_delete_premiums_perms = []
-    generate_invoice_on_contribution = None
 
     def __load_config(self, cfg):
         for field in cfg:
@@ -38,4 +27,3 @@ class ContributionConfig(AppConfig):
         from core.models import ModuleConfiguration
         cfg = ModuleConfiguration.get_or_default(MODULE_NAME, DEFAULT_CFG)
         self.__load_config(cfg)
-        read_all_calculation_rules()
